449d68f9410f49111de9ad351f05d0fe882566b9
fedebre <f.braga003@studenti.unibs.it>
2021-05-24 19:35:30 +0200

    creata qualche classe

---
+++ src/Città.java
@@ -0,0 +1,29 @@
+public class Città {
+
+    private Posizione posizione;
+    private int altezza;
+    private int id;
+
+    //getters e setters
+    public Posizione getPosizione() {
+        return posizione;
+    }
+    public void setPosizione(Posizione posizione) {
+        this.posizione = posizione;
+    }
+    public int getAltezza() {
+        return altezza;
+    }
+    public void setAltezza(int altezza) {
+        this.altezza = altezza;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+}
---
+++ src/LettoreXml.java
@@ -0,0 +1,19 @@
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class LettoreXml {
+
+       /* XMLInputFactory xmlif = null;
+        XMLStreamReader xmlr = null;
+        String filepath = "test_file/PgAr_Map_5.xml";
+
+        xmlif = XMLInputFactory.newInstance();
+        xmlr = xmlif.createXMLStreamReader("inputPersone", new FileInputStream(filepath));*/
+
+
+
+}
