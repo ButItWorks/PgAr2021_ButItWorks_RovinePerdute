90a83a7aa9d94a20c12a4d61560f07981abb2887
fedebre <f.braga003@studenti.unibs.it>
2021-05-25 22:55:46 +0200

    Create XmlUtilities.java

---
+++ src/XmlUtilities.java
@@ -0,0 +1,14 @@
+
+
+public class XmlUtilities {
+
+       /* XMLInputFactory xmlif = null;
+        XMLStreamReader xmlr = null;
+        String filepath = "test_file/PgAr_Map_5.xml";
+
+        xmlif = XMLInputFactory.newInstance();
+        xmlr = xmlif.createXMLStreamReader("inputPersone", new FileInputStream(filepath));*/
+
+
+
+}
